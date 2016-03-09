63adf359f74e7137ae70dcc1c0ed8a8346e977f4
Yueyue07 <xww.qyy@gmail.com>
2016-03-08 23:06:54 -0800

    Access-Control-Allow added

--- server/server.js
+++ server/server.js
@@ -9,10 +9,10 @@ mongoose.connect(process.env.MONGOLAB_URI
 
 
 app.use((req, res, next) => {
-  res.header('Access-Controller-Allow-Origin', 'http://localhost:5000');
-  res.header('Access-Controller-Allow-Headers',
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5000');
+  res.header('Access-Control-Allow-Headers',
     'Content-Type, Authorization, token');
-  res.header('Access-Controller-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   next();
 });
 
