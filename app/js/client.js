60eb2e31ea4179f0183aef006dbeec7304305f1e
Yueyue07 <xww.qyy@gmail.com>
2016-04-06 16:55:18 -0700

    use angular-ui-router as client-side router

--- app/js/client.js
+++ app/js/client.js
@@ -1,21 +1,19 @@
 const angular = require('angular');
-require('angular-route');
-require('angular-ui-route');
-const imageApp = angular.module('imageApp', ['ngRoute']);
+require('angular-ui-router');
+const imageApp = angular.module('imageApp', ['ui.router']);
 
 require(__dirname + '/image')(imageApp);
-require(__dirname + '/services')(imageApp);
 
-imageApp.config(['$routeProvider', function(routes) {
-  routes
-    .when('/', {
-      controller: 'ImageController',
-      templateUrl: '/views/home.html'
-    })
-    .when('/home', {
-      controller: 'ImageController',
-      templateUrl: '/views/home.html'
-    }).otherwise({
-      templateUrl: '/views/nofound.html'
-    });
-}]);
+
+imageApp.config(['$stateProvider', '$urlRouterProvider',
+  ($stateProvider, $urlRouterProvider) => {
+    $urlRouterProvider.when('', '/');
+    $urlRouterProvider.otherwise('/fourohfour');
+    $stateProvider
+      .state('home', {
+        url: '/',
+        controller: 'ImageController',
+        templateUrl: '/views/home.html'
+      });
+  }
+]);
